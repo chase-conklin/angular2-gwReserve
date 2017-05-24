52617be8488f95b720b07f83d87346cc2923393f
Chase Conklin <chase.conklin@outlook.com>
2017-05-23 20:38:35 -0700

    Inject components into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {LoginComponent} from './login/login.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {ExerciseComponent} from './exercise/exercise.component';
 
 @NgModule({
   imports      : [ BrowserModule ],
-  declarations : [ AppComponent ],
+  declarations : [ AppComponent, LoginComponent, NavigationComponent, ExerciseComponent ],
   bootstrap    : [ AppComponent ]
 })
 export class AppModule { }

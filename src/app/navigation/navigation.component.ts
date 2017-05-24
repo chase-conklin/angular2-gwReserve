de5572730eba7a463c84205ffc7ba200e98b8fb7
Chase Conklin <chase.conklin@outlook.com>
2017-05-23 21:04:16 -0700

    Start navigation items, end of class

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
 
+interface RoomInterface {
+  name:string,
+  capacity:number
+}
+
 @Component({
   selector : 'gw-navigation',
   templateUrl : './navigation.html'
 })
 
-export class NavigationComponent { }
+export class NavigationComponent {
+  private roomItems: Array<string> = ["Zelda", "Mario", "Kirby"];
+  private roomCapacity: Array<number> = [23, 33, 42];
+
+  public getNavItems() {
+
+  }
+}

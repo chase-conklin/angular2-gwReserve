746d099641d07a8092571a5ad9380edcdbe9c476
Chase Conklin <chase.conklin@outlook.com>
2017-05-25 16:05:22 -0700

    Created nav items from an array

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,7 +2,6 @@ import { Component } from "@angular/core";
 
 interface RoomInterface {
   name: string;
-  capacity: number;
 }
 
 @Component({
@@ -11,10 +10,5 @@ interface RoomInterface {
 })
 
 export class NavigationComponent {
-  private roomItems: Array<string> = ["Zelda", "Mario", "Kirby"];
-  private roomCapacity: Array<number> = [23, 33, 42];
-
-  public getNavItems() {
-
-  }
+  public roomNames: Array<string> = ["Zelda", "Mario", "Kirby"];
 }

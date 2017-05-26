a94cd42d3b23754364905a25a9e1f24cd0c6db88
Chase Conklin <chase.conklin@outlook.com>
2017-05-25 19:14:26 -0700

    First method of routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,50 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import {RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import {LoginComponent} from "./login/login.component";
 import {NavigationComponent} from "./navigation/navigation.component";
 import {ExerciseComponent} from "./exercise/exercise.component";
+import {RoomsComponent} from "./rooms/rooms.component";
+
+
+const routes: Routes = [
+  {
+    path: "exercises",
+    component: ExerciseComponent
+  },
+  {
+    path: "rooms/:id",
+    component: RoomsComponent
+  },
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "**",
+    pathMatch: "full",
+    redirectTo: ""
+  }
+];
 
 @NgModule({
-  imports      : [ BrowserModule ],
-  declarations : [ AppComponent, LoginComponent, NavigationComponent, ExerciseComponent ],
-  bootstrap    : [ AppComponent ]
+  imports      : [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations : [
+    AppComponent,
+    LoginComponent,
+    NavigationComponent,
+    ExerciseComponent,
+    RoomsComponent
+  ],
+  bootstrap    : [
+    AppComponent
+  ]
 })
 export class AppModule { }

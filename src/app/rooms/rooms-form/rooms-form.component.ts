014dfad145bb5346770c651d520fec91b913f4d9
Chase Conklin <chase.conklin@outlook.com>
2017-06-08 21:48:06 -0700

    Created a form, added some logic, injected deactivation guard

--- src/app/rooms/rooms-form/rooms-form.component.ts
+++ src/app/rooms/rooms-form/rooms-form.component.ts
@@ -1,14 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 import { IDirtyCanDeactivate } from "./../../services/dirty-form-guard.service";
+
 
 @Component({
   selector : "gw-rooms-form",
   templateUrl : "./rooms-form.component.html"
 })
 
-export class RoomsFormComponent implements IDirtyCanDeactivate {
+export class RoomsFormComponent implements IDirtyCanDeactivate, OnInit {
+  public reasons: string[];
+
+
+  @ViewChild("myForm") myForm: NgForm;
+
+  ngOnInit() {
+    this.reasons = ["Party", "Conference"];
+  }
+
+  onRoomSubmission(myForm: NgForm) {
+    console.log(myForm);
+  }
+
   dirtyCanDeactivate () {
-    return confirm("Do you want to leave?");
+    return this.myForm.dirty && !this.myForm.submitted ? confirm("You have unsaved work. Continue?") : true;
   }
 }
--- src/app/rooms/rooms.module.ts
+++ src/app/rooms/rooms.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
+import {FormsModule} from "@angular/forms";
 
 import {RoomRoutingModule} from "./rooms.routing.module";
 import {routedComponents} from "./rooms.routes";
 import {RoomsHeaderComponent} from "./rooms-header/rooms-header.component";
+import {CommonModule} from "@angular/common";
 
 
 @NgModule({
-  imports: [ RoomRoutingModule ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RoomRoutingModule ],
   declarations: [ routedComponents, RoomsHeaderComponent ]
 })
 

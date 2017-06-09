27475f01dbb3e30dc7cbc1b76310abbceb284815
Chase Conklin <chase.conklin@outlook.com>
2017-06-08 21:00:31 -0700

    Created bindings from parent to child and vice versa

--- src/app/exercise/exercise-child/exercise-child.component.ts
+++ src/app/exercise/exercise-child/exercise-child.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges} from "@angular/core";
 
 @Component({
-  selector: 'app-exercise-child',
-  templateUrl: './exercise-child.component.html',
-  styleUrls: ['./exercise-child.component.css']
+  selector: "app-exercise-child",
+  templateUrl: "./exercise-child.component.html",
+  styleUrls: ["./exercise-child.component.css"]
 })
-export class ExerciseChildComponent implements OnInit {
+export class ExerciseChildComponent implements OnInit, OnChanges {
+  @Input() public record;
+  @Output() public onEditRecord = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log("input binding changed", changes);
+  }
+
+  pretendEdit() {
+    console.log("editing...");
+
+    this.onEditRecord.emit(this.record + " passed by from child");
   }
 
 }
--- src/app/exercise/exercise.component.ts
+++ src/app/exercise/exercise.component.ts
@@ -1,8 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector : "gw-exercise",
   templateUrl : "./exercise.component.html"
 })
 
-export class ExerciseComponent { }
+export class ExerciseComponent implements OnInit {
+  public record: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.record = "Geekwise Angular Class";
+  }
+
+  onChildEdited(record) {
+    this.record = record + " RECEIVED BY PARENT";
+    console.log("putting record on database")
+  }
+}

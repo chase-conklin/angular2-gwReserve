8832d34870647b8d039aec4b4d4198da1c1adc03
Chase Conklin <chase.conklin@outlook.com>
2017-06-20 18:06:06 -0700

    Update rooms list

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,10 +1,48 @@
-import { Component, Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { RoomsService } from "./../../services/rooms.service";
+
+import { IRoom } from "./../../interfaces/IRoom";
+import { IReservation } from "./../../interfaces/IReservation";
 
 @Component({
-  selector : "gw-rooms-list",
-  templateUrl : "./rooms-list.component.html"
+  selector: "gw-room-list",
+  templateUrl: "./rooms-list.component.html"
 })
+export class RoomsListComponent implements OnInit {
+  public id: number;
+  public room: IRoom;
+  public reservations: IReservation[];
 
-export class RoomsListComponent {
-  @Input() id: String;
+  constructor(
+    private route: ActivatedRoute,
+    private roomService: RoomsService
+  ) { }
+
+  ngOnInit() {
+    this.route.parent.params
+      .map(params => params["id"])
+      .subscribe(id => {
+        this.id = id;
+        this.roomService.getRoomById(id).then(room => {
+          this.room = room;
+          this.reservations = this.getReservationsByDay();
+        });
+      });
+  }
+
+  getReservationsByDay(date = null) {
+    if (!this.room.reservations) return [];
+
+    const reservations = this.room.reservations[this.roomService.getRoomDateKey(date)] || [];
+
+    // as seen elsewhere in this application, we'd like reservations to have ids and for this
+    // to be the unique key generated for us in Firebase
+    for (let reservationKey in reservations) {
+      reservations[reservationKey] = reservationKey;
+    }
+
+    return reservations;
+  };
 }

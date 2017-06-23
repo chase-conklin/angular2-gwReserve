3d0faf9a8f18bd5b27a8f7d2371340ec6702ec90
Chase Conklin <chase.conklin@outlook.com>
2017-06-22 20:12:51 -0700

    Getting app ready to use animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
 
 import { NgModule } from "@angular/core";
@@ -25,6 +26,7 @@ import {ApplicationSettings} from "./services/application-settings.service";
 @NgModule({
   imports      : [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     AngularFireModule.initializeApp(environment.firebase),
